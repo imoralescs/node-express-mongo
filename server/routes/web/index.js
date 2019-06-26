f1ee717b3a4a2344255a808403d0a534bdf63189
Israel Morales <israel@savvycard.com>
2019-06-26 15:24:20 -0400

    comment

--- server/routes/web/index.js
+++ server/routes/web/index.js
@@ -21,4 +21,14 @@ router.get('/', (request, response) => {
         .catch(error => error)    
 })
 
+// @route GET /
+// @description Home
+// @access Public
+router.get('/create-note', (request, response) => {
+    response
+        .render('create_note', {
+            title: 'Create Note'
+        })   
+})
+
 module.exports = router;
